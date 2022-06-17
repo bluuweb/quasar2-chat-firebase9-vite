b87f23117cd6d3eaa34d1f428f9840f1432d8d11
bluuweb <contacto@bluuweb.cl>
2022-06-17 09:52:16 -0400

    🍉

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-layout view="hHr lpR fFr">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          QuasarChat
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="primary">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div>Footer</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const rightDrawerOpen = ref(false);
+
+const toggleRightDrawer = () => {
+  rightDrawerOpen.value = !rightDrawerOpen.value;
+};
+</script>
---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page padding>
+    <div class="q-pa-md row justify-center">
+      <div style="width: 100%; max-width: 400px">
+        <q-chat-message :text="['hey, how are you?']" sent name="me" />
+        <q-chat-message :text="['doing fine, how r you?']" name="cocodibuja" />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup></script>
